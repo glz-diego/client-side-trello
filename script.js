9818d128f72d999825e35a0bb2d6fdcbaad0b39e
glz-diego <glzdiego12@gmail.com>
2019-04-23 18:17:24 -0500

    init message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var SwimLane = [];
+
 function newTitle() {
 	var newT = document.getElementById("newTitle").value;
 	var title = document.getElementById("title");
@@ -14,11 +16,25 @@ function newTitle() {
 
 	display();
 }
+
+// function newName() {
+// 	var newN = document.getElementById("newName").value;
+// 	var name = document.getElementById("name");
+// 	name.remove(name.Index);
+// 	var add = document.createElement('h4');
+// 	add.innerHTML = newN;
+// 		add.style.color = "black";
+// 		add.style.fontSize = "20px";
+// 		add.style.fontFamily = "Roboto";
+// 	document.getElementById("").appendChild(add);
+// }
+
 function display(){
 	var box = document.getElementById("box");
 	box.style.display = "block";
 }
 function addSL(){
 	var box = document.getElementById("box");
-	box.insertAdjacentHTML("beforeend","<div class='SL'></div>");
+	var html = "<div class='SL' style='float: left;'><input type='text' placeholder='Card Name' reqired><input type='submit'></div>";
+	box.insertAdjacentHTML("beforeend",html);
 }
