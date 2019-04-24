49ec44bd4fc8766cb6541d5be6a8cd21cf37c930
glz-diego <glzdiego12@gmail.com>
2019-04-23 22:02:52 -0500

    init message

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ function display(){
 function addSL(){
 	var box = document.getElementById("box");
 	var html = "<div class='SL' style='float: left; margin: 7px' id='list'><h6 id='name'><input type='text' id='newName' placeholder='SwimLane Name' reqired><input type='submit' onclick='newName()' value='Add Name' class='btn btn-secondary'></h6></div>";
-	box.insertAdjacentHTML("beforeend",html);
-	// newName();
+	box.insertAdjacentHTML("beforeend", html);
 }
 
 function newName() {
@@ -40,13 +39,17 @@ function newName() {
 		add.style.fontFamily = "Roboto";
 		add.style.textAlign = "center";
 	document.getElementById("list").appendChild(add);
-	addCard();
+	addButton();
 }
-
-function addCard(){
+var aC;
+function addButton(){
 	var list = document.getElementById("list");
-	var html = "<input type='submit' value='Add Card' onclick='prompt()' class='btn btn-secondary'>"
-	list.insertAdjacentHTML("beforeend", html);
-	// var card = "<div class='form-group'><textarea class='form-control' id='exampleFormControlTextarea1' rows='3'></textarea></div>"
-	// document.getElementById("list").appendChild(card);
+	aC = "<input type='submit' value='Add Card' onclick='addCard()' class='btn btn-secondary'>"
+	list.insertAdjacentHTML("beforeend", aC);
+	return aC;
 }
+	function addCard(){
+		aC.remove(aC.Index);
+		var card = "<input type='text' value='Card Name'><input type='submit' value='Add Name' class='btn btn-secondary'>"
+		document.getElementById("list").appendChild(card);
+	}
