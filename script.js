4bde7ea9ae87d6de7a5b3e7792d07614b8ba0921
glz-diego <glzdiego12@gmail.com>
2019-04-23 21:28:21 -0500

    init message

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function display(){
 
 function addSL(){
 	var box = document.getElementById("box");
-	var html = "<div class='SL' style='float: left; margin: 7px' id='list'><h6 id='name'><input type='text' id='newName' placeholder='SwimLane Name' reqired><input type='submit' onclick='newName()'></h6></div>";
+	var html = "<div class='SL' style='float: left; margin: 7px' id='list'><h6 id='name'><input type='text' id='newName' placeholder='SwimLane Name' reqired><input type='submit' onclick='newName()' value='Add Name' class='btn btn-secondary'></h6></div>";
 	box.insertAdjacentHTML("beforeend",html);
 	// newName();
 }
@@ -40,8 +40,17 @@ function newName() {
 		add.style.fontFamily = "Roboto";
 		add.style.textAlign = "center";
 	document.getElementById("list").appendChild(add);
+	addCard();
 }
 
 function addCard(){
-
+	var list = document.getElementById("list");
+	var html = "<input type='submit' value='Add Card' onclick='prompt()' class='btn btn-secondary'>"
+	box.insertAdjacentHTML("beforeend",html);
+	// var card = "<div class='form-group'><textarea class='form-control' id='exampleFormControlTextarea1' rows='3'></textarea></div>"
+	// document.getElementById("list").appendChild(card);
+}
+function prompt(){
+	var cTitle = prompt("Name your card:");
+	var cDescription = prompt("Description of your task:");
 }
