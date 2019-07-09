0a11dfc5f937a20ba956130c7889ce47d7f72d4c
glz-diego <glzdiego12@gmail.com>
2019-07-08 21:19:47 -0500

    init message

--- script.js
+++ script.js
@@ -31,7 +31,11 @@ function addSL(){
 			<input type='submit' onclick='newName(event, this, ${swimlaneCounter})' value='Add Name' class='btn btn-secondary'>
 		</h6>
 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalFor-${swimlaneCounter}"> Delete? </button>
+		<button onclick="move('left',${swimlaneCounter})"><i class="fas fa-arrow-left"></i></button>
+		<button onclick="move('right',${swimlaneCounter})"><i class="fas fa-arrow-right"></i></button>
 		<button class="btn btn-success" onclick="createCard(${swimlaneCounter})">Add Card +</button>
+
+		<div class='cardContainer'> </div>
 		
 		<div class="modal fade" id="modalFor-${swimlaneCounter}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 			<div class="modal-dialog" role="document">
@@ -89,9 +93,13 @@ function deleteItem(itemType, itemId){
 }
 
 function createCard(swimlaneId){
-	var list = document.getElementById(`list-${swimlaneId}`);
+	var list = document.getElementById(`list-${swimlaneId}`)
 	var aC = 
 	`<div class='cardBox' id="card-${cardCounter}">
+			<button onclick="moveCard('up',${cardCounter})"><i class="fas fa-arrow-up"></i></button>
+			<button onclick="moveCard('right',${cardCounter})"><i class="fas fa-arrow-right"></i></button>
+			<button onclick="moveCard('down',${cardCounter})"><i class="fas fa-arrow-down"></i></button>
+			<button onclick="moveCard('left',${cardCounter})"><i class="fas fa-arrow-left"></i></button>
 		<div class="form-group">
 			<input type="text" placeholder='Card Name' class="form-control" id="cName-${cardCounter}" style='width: 150px; float: left;'>
 			<input type='submit' id="addCardButton-${cardCounter}" value='Add' onclick='addCard(${cardCounter})' class='btn btn-secondary' style='margin: 0px 0px 7px 7px !important'>
@@ -99,7 +107,7 @@ function createCard(swimlaneId){
 		</div>
 	</div>`;
 
-	list.insertAdjacentHTML('beforeend', aC);
+	list.children[5].insertAdjacentHTML('beforeend', aC);
 	cardCounter++;
 }
 
@@ -121,4 +129,83 @@ function addCard(cardId){
 	card.insertAdjacentHTML("beforeend", cNameText);
 	card.insertAdjacentHTML("beforeend", cDescText);
 	card.insertAdjacentHTML("beforeend", deleteBtn);
+}
+
+function move(direction, swimlaneId){
+	const swimlaneListId = `list-${swimlaneId}`;
+
+	const swimlane = document.getElementById(swimlaneListId);
+	const container = swimlane.parentNode.parentNode;
+
+	let index;
+
+	for (var i = 0; i < container.children.length; i++) {
+		// finds the swimlane by id
+		if(swimlaneListId === container.children[i].firstChild.id){
+			index = i;
+		}
+	}
+
+	if (index === undefined) {
+		console.log("fail")
+	} else {
+		if (direction.toUpperCase() === "LEFT") container.insertBefore(swimlane.parentNode, container.children[index-1]);
+		if (direction.toUpperCase() === "RIGHT") container.insertBefore(swimlane.parentNode, container.children[index+2]);
+		if (!["RIGHT", "LEFT"].includes(direction.toUpperCase())) console.log("invalid direction");
+	}
+}
+
+function moveCard(direction, cardId){
+	const cardListId = `card-${cardId}`;
+
+	const card = document.getElementById(cardListId);
+	const swimlane = card.parentNode.parentNode;
+
+	const swimlaneListId = swimlane.id;
+
+	const swimlaneContainer = swimlane.parentNode.parentNode;
+	const cardContainer = card.parentNode;
+
+	let cardIndex;
+	let swimlaneIndex;
+
+	for (let i = 0; i < cardContainer.children.length; i++) {
+		// finds the card by id
+		if(cardListId === cardContainer.children[i].id){
+			cardIndex = i;
+		}
+	}
+
+	for (let i = 0; i < swimlaneContainer.children.length; i++) {
+		// finds the swimlane by id
+		if(swimlaneListId === swimlaneContainer.children[i].firstChild.id){
+			swimlaneIndex = i;
+		}
+	}
+
+
+	if (cardIndex === undefined && swimlaneIndex === undefined) {
+		console.log("fail")
+	} else {
+		if (direction.toUpperCase() === "UP") cardContainer.insertBefore(card, cardContainer.children[cardIndex-1]);
+		if (direction.toUpperCase() === "DOWN") cardContainer.insertBefore(card, cardContainer.children[cardIndex+2]);
+
+		if (direction.toUpperCase() === "RIGHT") {
+			const swimlaneToTheRight = swimlaneContainer.children[swimlaneIndex+1].firstChild;
+
+			const cardContainerToTheRight = swimlaneToTheRight.children[5];
+
+			cardContainerToTheRight.appendChild(card);
+		}
+		if (direction.toUpperCase() === "LEFT") {
+			const swimlaneToTheLeft = swimlaneContainer.children[swimlaneIndex-1].firstChild;
+
+			const cardContainerToTheLeft = swimlaneToTheLeft.children[5];
+
+			cardContainerToTheLeft.appendChild(card);
+		}
+
+
+		if (!["UP", "DOWN", "RIGHT", "LEFT"].includes(direction.toUpperCase())) console.log("invalid direction");
+	}
 }
